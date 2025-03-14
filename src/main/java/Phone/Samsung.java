1107a506a824649818ecd22c773746722793e734
MarioGIT-lab <mariorocx1234@yahoo.com>
2025-03-14 12:29:02 +0200

    Samsung.java

--- src/main/java/Phone/Samsung.java
+++ src/main/java/Phone/Samsung.java
@@ -2,6 +2,6 @@ package Phone;
 
 public abstract class Samsung extends Phone {
     public Samsung(Color color, Material material, String IMEI) {
-        super(color, material, IMEI, 19); // Samsung phones start with 19% battery
+        super(color, material, IMEI, 19);
     }
 }
